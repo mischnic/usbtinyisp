2c29781711e9c13a12087059f290fa6a57c7885c
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2017-12-29 21:04:44 +0100

    Add option for additional delay

--- spislave.js
+++ spislave.js
@@ -51,48 +51,54 @@ SPISlave.parseCmd = (v) =>
 
 function SPISlave(){
 	this.avr = new AVR();
+	this.opened = false;
 }
 
 SPISlave.prototype.open = async function(){
 	await this.avr.open();
 	await this.avr.powerUp();
+	this.opened = true;
 };
 
 SPISlave.prototype.close = async function(){
-	await this.avr.powerDown();
+	if(this.opened){
+		await this.avr.powerDown();
+	}
 	await this.avr.close();
 };
 
-SPISlave.prototype.send = async function(cmd, n){
+SPISlave.prototype.send = async function(cmd, n, addDelay){
+	addDelay = addDelay || 0;
 	await this.avr.spi1(cmd);
-	await delay(10);
+	await delay(1 + addDelay);
 
 	let val = Buffer.from([]);
 
 	for (let i = 0; i < n; i++) {
 		val = Buffer.concat([val, await this.avr.spi1(0)]);
+		await delay(1);
 	}
 
 	return val;
 };
 
-SPISlave.prototype.getByte = async function(cmd){
-	const buffer = await this.send(SPISlave.parseCmd(cmd), 1);
+SPISlave.prototype.getByte = async function(cmd, addDelay){
+	const buffer = await this.send(SPISlave.parseCmd(cmd), 1, addDelay);
 	return buffer[0];
 };
 
-SPISlave.prototype.getInt = async function(cmd){
-	const buffer = await this.send(SPISlave.parseCmd(cmd), 2);
+SPISlave.prototype.getInt = async function(cmd, addDelay){
+	const buffer = await this.send(SPISlave.parseCmd(cmd), 2, addDelay);
 	return buffer[0] | buffer[1] << 8;
 };
 
-SPISlave.prototype.getLong = async function(cmd){
-	const buffer = await this.send(SPISlave.parseCmd(cmd), 4);
+SPISlave.prototype.getLong = async function(cmd, addDelay){
+	const buffer = await this.send(SPISlave.parseCmd(cmd), 4, addDelay);
 	return buffer[0] | buffer[1] << 8 | buffer[2] << 16 | buffer[3] << 24;
 };
 
-SPISlave.prototype.getFloat = async function(cmd){
-	const buffer = await this.send(SPISlave.parseCmd(cmd), 4);
+SPISlave.prototype.getFloat = async function(cmd, addDelay){
+	const buffer = await this.send(SPISlave.parseCmd(cmd), 4, addDelay);
 	return buffer.readFloatLE(0);
 };
 
